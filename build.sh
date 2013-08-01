0d8b83a3006f34bd377c7872fc53ee3dabc303fa
Joe Gornick <joe@joegornick.com>
2013-08-01 00:14:33 -0500

    Cleaning out the gh-pages branch locally before we copy the built site content.

--- build.sh
+++ build.sh
@@ -19,6 +19,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_REPO_SLUG" == "jgornick/r
   # Fetch all other branches
   git fetch origin 'refs/heads/*:refs/remotes/origin/*' || error_exit "Error fetching remote branches";
   git checkout -b gh-pages origin/gh-pages || error_exit "Error checking out gh-pages branch";
+  git clean -fd
   cp -Rf _site/* . || error_exit "Error copying _site contents to gh-pages";
   git add .
   git add -u
